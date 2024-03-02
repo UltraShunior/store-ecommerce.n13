daba58a21f6211fc3196a99ffd3db955859c3b41
Nahuel Bogado <ultrashunior@gmail.com>
2024-03-01 23:33:09 -0300

    update product card

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -41,12 +41,12 @@ const ProductCard: React.FC<ProductCard> = ({
   return ( 
     <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
       {/* Image & actions */}
-      <div className="aspect-square rounded-xl bg-gray-100 relative">
+      <div className="aspect-square bg-gray-100 relative">
         <Image 
           src={data.images?.[0]?.url} 
           alt="" 
           fill
-          className="aspect-square object-cover rounded-md"
+          className="object-cover rounded-md"
         />
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
